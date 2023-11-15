4587667684e881d52554ec7a7aef94489a8027bd
Francisco Neto <wantedshark@gmail.com>
2023-11-15 14:57:39 -0300

    cart text changed

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -14,17 +14,18 @@ export default function Cart({ handleCloseCart, handleSetMsg }) {
     const [address, setAddress] = useState(addressAvailables[0])
     const [cupom, setCupom] = useState("")
     const [price, setPrice] = useState(0)
-    const validCupom = "INDICAÇÃO5";
 
-    const text = cart.map(item => (
-        `•%20${item.name},%20cor:%20${item.color},%20tam:%20${item.size},%20qnt:${item.quant}`
+    const items = cart.map(item => (
+        `*${item.name}*%0A*Tamanho:*%20${item.size}%0A*Cor:*%20${item.color}%0A*Quantidade:*%20${item.quant}%0A*Valor:*%20R$%20${item.price + address.price - 1 +",90"}`
     )).join("%0A");
+
+    const text = `------------------------------%0A%20%20%20%20*Novo%20Pedido*%0A------------------------------%0A%0A${items}%0A%0A*Endereço:*%20${address.name}%0A*Frete:*%20R$%20${address.price === 0 ? "Grátis" : address.price}*Cupom:*%20${cupom === "INDICAÇÃO5" ? "INDICAÇÃO5" : "nenhum" }%0A*Total:*%20R$%20${price - 1 + address.price},90`
 
     function handleCloseBtn() {
         setClosing(true)
         handleCloseCart()
     }
-    
+
 
     function handleRemoveItem(name) {
         const newCart = cart.filter(i => i.name !== name)
@@ -114,7 +115,7 @@ export default function Cart({ handleCloseCart, handleSetMsg }) {
                                     <div className="cart_body_cupom">
                                         <input value={cupom} onChange={e => setCupom(e.target.value.toUpperCase())} type="text" placeholder="cupom de desconto" autoComplete="none" />
                                     </div>
-                                    <a href={`https://wa.me/5585996585581?text=Olá%20gostaria%20de%20fazer%20um%20pedido:%0A${text}%0A%0AEndereço:%20${address.name}%0ACupom:${cupom === validCupom ? cupom : "nenhum"}`} className="cart_body_button">Finalizar compra</a>
+                                    <a href={`https://wa.me/5585996585581?text=${text}`} className="cart_body_button" onClick={() => console.log(text)} target="_blank">Finalizar compra</a>
                                 </div>
                             </> :
                             <>
